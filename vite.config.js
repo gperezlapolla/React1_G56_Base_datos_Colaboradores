c680ff4d864cb9a003c770f53f122f5e10360459
Guillermo Perez Lapolla <141288383+gperezlapolla@users.noreply.github.com>
2025-12-18 15:30:00 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Consumo-APIs-con-React-G56-Examen-final/',
 })
